8d7a06f63abfed0d913d5a6cc92b2bde9175a18f
aris <ariskou@gmail.com>
2016-06-10 02:35:22 +0300

    Impored new empty modules into Web.Skroutz.Types.

--- src/Web/Skroutz/Types.hs
+++ src/Web/Skroutz/Types.hs
@@ -7,9 +7,22 @@
 -- Stability   :  alpha
 -- Portability :
 --
--- Provides the Skroutz API Types for JSON marshalling
+-- Provides the Skroutz API 'Types' for JSON marshalling.
 ----------------------------------------------------------------------------
 
 module Web.Skroutz.Types
   (
   ) where
+
+
+import Web.Skroutz.Types.AnySearchResult
+import Web.Skroutz.Types.Category
+import Web.Skroutz.Types.Manufacturer
+import Web.Skroutz.Types.Meta
+import Web.Skroutz.Types.Pagination
+import Web.Skroutz.Types.Product
+import Web.Skroutz.Types.Shop
+import Web.Skroutz.Types.ShopLocation
+import Web.Skroutz.Types.ShopReview
+import Web.Skroutz.Types.SKU
+import Web.Skroutz.Types.SKUReview
