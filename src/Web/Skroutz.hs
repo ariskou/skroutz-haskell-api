1a023980fbc73c0f80fa15877890bb0a65dbc549
aris <ariskou@gmail.com>
2016-06-10 02:33:19 +0300

    Added copyright, configuration and some empty code files.
    Added copyright NOTICE.
    Added changelog.
    Added .gitignore.
    Added package.yaml for hpack.
    Added Setup.hs and hpack-generated skroutz-haskell-api.cabal.
    Added Web.Skroutz which will contain the public interface.
    Added Web.Skroutz.Types which will contain the data model definitions.

---
+++ src/Web/Skroutz.hs
@@ -0,0 +1,17 @@
+----------------------------------------------------------------------------
+-- |
+-- Module      :  Web.Skroutz
+-- Copyright   :  (c) 2016 Remous-Aris Koutsiamanis
+-- License     :  Apache License 2.0
+-- Maintainer  :  Remous-Aris Koutsiamanis <ariskou@gmail.com>
+-- Stability   :  alpha
+-- Portability :
+--
+-- Provides the Skroutz API functions
+----------------------------------------------------------------------------
+
+module Web.Skroutz
+  (
+  ) where
+
+import qualified Web.Skroutz.Types           as Types
---
+++ src/Web/Skroutz/Types.hs
@@ -0,0 +1,15 @@
+----------------------------------------------------------------------------
+-- |
+-- Module      :  Web.Skroutz.Types
+-- Copyright   :  (c) 2016 Remous-Aris Koutsiamanis
+-- License     :  Apache License 2.0
+-- Maintainer  :  Remous-Aris Koutsiamanis <ariskou@gmail.com>
+-- Stability   :  alpha
+-- Portability :
+--
+-- Provides the Skroutz API Types for JSON marshalling
+----------------------------------------------------------------------------
+
+module Web.Skroutz.Types
+  (
+  ) where
