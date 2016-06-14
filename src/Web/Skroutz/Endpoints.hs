d13634e9b7c4720a8f640138b1c462fe292f1648
aris <ariskou@gmail.com>
2016-06-14 13:59:23 +0300

    Added empty Web.Skroutz.Endpoints.

--- src/Web/Skroutz/Endpoints.hs
+++ src/Web/Skroutz/Endpoints.hs
@@ -1,18 +1,15 @@
 ----------------------------------------------------------------------------
 -- |
--- Module      :  Web.Skroutz
+-- Module      :  Web.Skroutz.Endoints
 -- Copyright   :  (c) 2016 Remous-Aris Koutsiamanis
 -- License     :  Apache License 2.0
 -- Maintainer  :  Remous-Aris Koutsiamanis <ariskou@gmail.com>
 -- Stability   :  alpha
 -- Portability :
 --
--- Provides the Skroutz API functions
+-- Provides the Skroutz API endpoints/methods.
 ----------------------------------------------------------------------------
-module Web.Skroutz
-  (
-    module X
-  ) where
+module Web.Skroutz.Endoints
+where
 
-import           Web.Skroutz.Endpoints as X
-import           Web.Skroutz.Types     as X
+import           Web.Skroutz.Types
