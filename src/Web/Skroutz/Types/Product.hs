4c7872d9cd8f9cf388775fc705ee10ec8f7bb335
aris <ariskou@gmail.com>
2016-06-19 16:59:42 +0300

    Added model data definition in Web.Skroutz.Types.Product.

--- src/Web/Skroutz/Types/Product.hs
+++ src/Web/Skroutz/Types/Product.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DeriveGeneric   #-}
+{-# LANGUAGE TemplateHaskell #-}
 ----------------------------------------------------------------------------
 -- |
 -- Module      :  Web.Skroutz.Types.Product
@@ -11,3 +13,38 @@
 ----------------------------------------------------------------------------
 module Web.Skroutz.Types.Product
 where
+
+import           Data.Text
+import           GHC.Generics           (Generic)
+import           Web.Skroutz.TH
+import           Web.Skroutz.Types.Meta
+import           Web.Skroutz.Types.URI
+
+data Product = Product {
+    _productId           :: Int
+  , _productName         :: Text
+  , _productSkuId        :: Int
+  , _productShopId       :: Int
+  , _productCategoryId   :: Int
+  , _productAvailability :: Text
+  , _productClickUrl     :: URI
+  , _productShopUid      :: Text
+  , _productExpenses     :: Maybe Text --TODO: Check for real type, only null examples offered
+  , _productWebUri       :: URI
+  , _productPrice        :: Double
+  } deriving (Generic, Show)
+
+makeLensesAndJSON ''Product "_product"
+
+data SingleProductResponse = SingleProductResponse {
+    _singleProductResponseProduct :: Product
+  } deriving (Generic, Show)
+
+makeLensesAndJSON ''SingleProductResponse "_singleProductResponse"
+
+data MultipleProductResponse = MultipleProductResponse {
+    _multipleProductResponseCategories :: [Product]
+  , _multipleProductResponseMeta       :: Meta
+  } deriving (Generic, Show)
+
+makeLensesAndJSON ''MultipleProductResponse "_multipleProductResponse"
