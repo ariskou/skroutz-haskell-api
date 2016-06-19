7113ea9e969f936c8c907a61301b672942eb2843
aris <ariskou@gmail.com>
2016-06-19 20:22:21 +0300

    Added model data definition in Web.Skroutz.Types.ShopReview.

--- src/Web/Skroutz/Types/ShopReview.hs
+++ src/Web/Skroutz/Types/ShopReview.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DeriveGeneric   #-}
+{-# LANGUAGE TemplateHaskell #-}
 ----------------------------------------------------------------------------
 -- |
 -- Module      :  Web.Skroutz.Types.ShopReview
@@ -11,3 +13,26 @@
 ----------------------------------------------------------------------------
 module Web.Skroutz.Types.ShopReview
 where
+
+import           Data.Text
+import           GHC.Generics           (Generic)
+import           Web.Skroutz.TH
+import           Web.Skroutz.Types.Meta
+
+data ShopReview = ShopReview {
+    _shopReviewId        :: Int
+  , _shopReviewUserId    :: Int
+  , _shopReviewReview    :: Text
+  , _shopReviewRating    :: Int
+  , _shopReviewShopReply :: Maybe Text
+  , _shopReviewCreatedAt :: Text
+  } deriving (Generic, Show)
+
+makeLensesAndJSON ''ShopReview "_shopReview"
+
+data MultipleShopReviewResponse = MultipleShopReviewResponse {
+    _multipleShopReviewResponseLocation :: [ShopReview]
+  , _multipleShopReviewResponseMeta     :: Meta
+  } deriving (Generic, Show)
+
+makeLensesAndJSON ''MultipleShopReviewResponse "_multipleShopReviewResponse"
