189e8f950fd43dcc4f04de1a8da7434bbb5fec7d
aris <ariskou@gmail.com>
2016-09-23 22:36:06 +0300

    Replaced Text with Alternative field in Meta.

--- src/Web/Skroutz/Model/ResultWrappers/Meta.hs
+++ src/Web/Skroutz/Model/ResultWrappers/Meta.hs
@@ -21,19 +21,20 @@ import           Data.Data                                         (Data,
                                                                     Typeable)
 import           Data.Text                                         (Text)
 import           GHC.Generics                                      (Generic)
-import           Web.Skroutz.TH
+import           Web.Skroutz.Model.ResultWrappers.Alternative
 import           Web.Skroutz.Model.ResultWrappers.AppliedFilters
 import           Web.Skroutz.Model.ResultWrappers.AvailableFilters
 import           Web.Skroutz.Model.ResultWrappers.Pagination
 import           Web.Skroutz.Model.ResultWrappers.StrongMatches
+import           Web.Skroutz.TH
 
 data Meta = Meta {
     _metaPagination       :: Pagination
-  , _metaOrderedBy        :: Maybe Text
+  , _metaOrderedBy        :: Maybe Text -- "popularity", "price", "pricevat", "rating"
   , _metaAppliedFilters   :: Maybe AppliedFilters
   , _metaAvailableFilters :: Maybe AvailableFilters
   , _metaQ                :: Maybe Text
-  , _metaAlternatives     :: Maybe [Text]
+  , _metaAlternatives     :: Maybe [Alternative]
   , _metaStrongMatches    :: Maybe StrongMatches
   } deriving (Eq, Ord, Typeable, Data, Generic, Show, NFData)
 
