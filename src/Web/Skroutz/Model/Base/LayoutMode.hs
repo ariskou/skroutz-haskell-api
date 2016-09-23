bedc0ef87b4a9bc570b056f29987482b3380fa19
aris <ariskou@gmail.com>
2016-09-24 02:17:22 +0300

    Added Web.Skroutz.Model.Base.LayoutMode

---
+++ src/Web/Skroutz/Model/Base/LayoutMode.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE DeriveAnyClass        #-}
+{-# LANGUAGE DeriveDataTypeable    #-}
+{-# LANGUAGE DeriveGeneric         #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TemplateHaskell       #-}
+----------------------------------------------------------------------------
+-- |
+-- Module      :  Web.Skroutz.Model.Base.Gender
+-- Copyright   :  (c) 2016 Remous-Aris Koutsiamanis
+-- License     :  Apache License 2.0
+-- Maintainer  :  Remous-Aris Koutsiamanis <ariskou@gmail.com>
+-- Stability   :  alpha
+-- Portability :
+--
+-- Provides the 'LayoutMode' type, a type representing the supported layout modes for 'Category'.
+----------------------------------------------------------------------------
+module Web.Skroutz.Model.Base.LayoutMode
+where
+
+import           Control.DeepSeq (NFData)
+import           Data.Data       (Data, Typeable)
+import           GHC.Generics    (Generic)
+import           Web.Skroutz.TH
+
+data LayoutMode = LayoutModeList | LayoutModeTiles | LayoutModeWideTiles
+  deriving (Eq, Ord, Typeable, Data, Generic, Show, NFData)
+
+makeLensesAndJSONSumType ''LayoutMode "" "LayoutMode"
