190cd42ecf59e72b9985f21bf5d1840f65c7f412
aris <ariskou@gmail.com>
2016-09-28 15:33:37 +0300

    Updated Web.Skroutz.FixtureGenerator to use Web.Skroutz.ApiEntries

--- test/Web/Skroutz/FixtureGenerator.hs
+++ test/Web/Skroutz/FixtureGenerator.hs
@@ -1,6 +1,8 @@
-{-# LANGUAGE ExplicitForAll      #-}
-{-# LANGUAGE OverloadedStrings   #-}
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE GADTs                 #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE PartialTypeSignatures #-}
 ----------------------------------------------------------------------------
 -- |
 -- Module      :  Web.Skroutz.FixtureGenerator
@@ -14,7 +16,7 @@
 ----------------------------------------------------------------------------
 module Web.Skroutz.FixtureGenerator
 (
-  generate
+  generateFixtures
 )
 where
 
@@ -22,7 +24,7 @@ import           Control.Monad.Trans.Except (runExceptT)
 import qualified Data.ByteString            as B
 import qualified Data.ByteString.Lazy       as BL
 import           Data.Either.Combinators    (fromRight')
-import           Data.Foldable              (traverse_)
+import           Data.HList
 import           Data.Text                  (pack)
 import           Data.Text.Encoding         (encodeUtf8)
 import           Network.HTTP.Client
@@ -31,6 +33,7 @@ import           System.Directory           (getCurrentDirectory)
 import           System.Environment         (getEnv)
 import           System.FilePath            ((</>))
 import qualified Web.Skroutz                as Skroutz
+import           Web.Skroutz.ApiEntries     (apiEntries)
 
 apiIdentifierEnvKey :: String
 apiIdentifierEnvKey = "API_IDENTIFIER"
@@ -40,56 +43,6 @@ apiSecretEnvKey = "API_SECRET"
 
 apiEntrypoint :: String
 apiEntrypoint = "http://api.skroutz.gr"
-
-apiEntries :: [(String, String)]
-apiEntries = [
-    ("categories_index", "/categories")
-  , ("categories_root", "/categories/root")
-  , ("categories_show", "/categories/1442")
-  , ("children_api_category", "/categories/1434/children") -- is 73 in the docs, but that category does not exist
-  , ("category_manufacturers", "/categories/25/manufacturers")
-  , ("category_skus", "/categories/40/skus")
-  , ("category_parent", "/categories/1442/parent")
-  , ("shops_show", "/shops/452")
-  , ("shop_locations_index", "/shops/452/locations")
-  , ("shop_locations_index_embed_address", "/shops/452/locations?embed=address")
-  , ("shop_locations_show", "/shops/452/locations/2500")
-  , ("shop_locations_show_embed_address", "/shops/452/locations/2500?embed=address")
-  , ("manufacturers_show", "/manufacturers/12907")
-  , ("manufacturers_index", "/manufacturers")
-  , ("manufacturer_categories", "/manufacturers/356/categories")
-  , ("manufacturer_skus", "/manufacturers/356/skus")
-  , ("autocomplete", "/autocomplete?q=iph")
-  , ("search_invalid", "/search?q=a")
-  , ("search_category_match", "/search?q=Tablets")
-  , ("search_manufacturer_match", "/search?q=apple")
-  , ("search_latin_mapping", "/search?q=ιπηονε")
-  , ("search_spelling", "/search?q=ipone")
-  , ("search_drop", "/search?q=wrong+iphone")
-  , ("search_no_results", "/search?q=asdf")
-  , ("search_category_facade", "/search?q=iphone")
-  , ("search_sku_match", "/search?q=nikon+1+j2")
-  , ("search_skus_match", "/search?q=samsung+galaxy+s5+16GB")
-  , ("skus_show", "/skus/3690169")
-  , ("skus_reviews", "/skus/3690169/reviews")
-  , ("skus_products", "/skus/3783654/products")
-  , ("skus_similar", "/skus/3034682/similar")
-  , ("skus_price_history", "/skus/3034682/price_history")
-  , ("products_show", "/products/12176638")
-  , ("products_search", "/shops/452/products/search?shop_uid=95F")
-  , ("filter_groups_index", "/categories/40/filter_groups")
-  , ("category_skus_search", "/categories/40/skus?q=iphone")
-  , ("category_skus_manufacturers", "/categories/40/skus?manufacturer_ids%5B%5D=28&manufacturer_ids%5B%5D=2")
-  , ("category_skus_filters", "/categories/40/skus?filter_ids%5B%5D=355559&filter_ids%5B%5D=6282")
-  , ("category_skus_with_available_filters", "/categories/279/skus?include_meta=available_filters")
-  , ("category_skus_with_applied_filters", "/categories/40/skus?include_meta=applied_filters&filter_ids%5B%5D=6282&manufacturer_ids%5B%5D=28")
-  , ("sku_reviews", "/skus/355766/reviews")
-  , ("sku_specifications", "/skus/2588170/specifications")
-  , ("sku_specifications_include_group", "/skus/2588170/specifications?include=group")
-  , ("shop_reviews", "/shops/452/reviews") -- is 3 in the docs, but that shop has no reviews
-  , ("shop_search", "/shops/search?q=spartoo")
-  , ("flags_index", "/flags")
-  ]
 
 getAuthToken :: IO B.ByteString
 getAuthToken = do
@@ -124,7 +77,12 @@ saveFixture authToken fixtureName fixtureApiPath = do
   fixtureDir <- getFixtureDir
   BL.writeFile (fixtureDir </> fixtureName ++ ".json") (responseBody response)
 
-generate :: IO ()
-generate = do
+data HSaveFixtureT = HSaveFixtureT B.ByteString
+
+instance (from ~ (String, String, _a), to ~ IO () ) => ApplyAB HSaveFixtureT from to where
+  applyAB (HSaveFixtureT authToken) (fixtureName, fixtureApiPath, _) = saveFixture authToken fixtureName fixtureApiPath
+
+generateFixtures :: IO ()
+generateFixtures = do
   authToken <- getAuthToken
-  traverse_ (uncurry $ saveFixture authToken) apiEntries
+  hMapM_ (HSaveFixtureT authToken) apiEntries
